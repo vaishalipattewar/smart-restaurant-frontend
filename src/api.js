9f47bf5dab0385db6376cf1b42a2396dccea5923
vaishalipattewar <pattewar.vaishali@gmail.com>
2026-07-22 09:02:23 +0000

    Updated Base URL to use dynamic url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+const API = axios.create({
+  baseURL: process.env.REACT_APP_API_BASE || "http://localhost:5000/api"
 });
 
 api.interceptors.request.use((config) => {

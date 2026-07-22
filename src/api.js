229109aa63ca776e9b6d3c8dd0688a43951d4597
vaishalipattewar <pattewar.vaishali@gmail.com>
2026-07-22 09:24:14 +0000

    Updated Base URL to use dynamic url

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({
+const api = axios.create({
   baseURL: process.env.REACT_APP_API_BASE || "http://localhost:5000/api"
 });
 
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+// Define process.env.REACT_APP_API_BASE at build time so browser code can use it
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    define: {
+      'process.env.REACT_APP_API_BASE': JSON.stringify(process.env.REACT_APP_API_BASE || 'http://localhost:5000/api')
+    }
+  }
 })
